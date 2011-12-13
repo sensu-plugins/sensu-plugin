b668273f47383665eef3d84f9dadaa883e6594af
Decklin Foster <decklin@red-bean.com>
2011-12-13 18:01:59 -0500

    Recover from exceptions in checks

--- lib/sensu-plugin/cli.rb
+++ lib/sensu-plugin/cli.rb
@@ -43,9 +43,13 @@ module Sensu
       end
 
       at_exit do
-        check = @@autorun.new
-        check.parse_options
-        check.run
+        begin
+          check = @@autorun.new
+          check.parse_options
+          check.run
+        rescue Exception => e
+          critical "Check failed to run: #{e}"
+        end
         warning "Check did not exit! You should call an exit code method."
       end
 
