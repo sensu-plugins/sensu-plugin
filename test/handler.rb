834de86f4560644722e08e76191077d9fcac2e7c
Decklin Foster <decklin@red-bean.com>
2011-12-21 17:21:31 -0500

    Add test for handler with no filtering, prints event

---
+++ test/handler.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'minitest/autorun'
+require 'json'
+
+class TestHandler < MiniTest::Unit::TestCase
+
+  def setup
+    @script = File.join(File.dirname(__FILE__), 'handlers/handle-nofilter')
+  end
+
+  def run_script(event)
+    output = IO.popen(@script, 'r+') do |child|
+      child.puts JSON.generate(event)
+      child.close_write
+      child.read
+    end
+  end
+
+  def test_handled
+    event = {
+      'client' => { 'name' => 'test' },
+      'check' => { 'name' => 'test' },
+      'occurrences' => 1,
+    }
+    output = run_script(event)
+    assert $?.exitstatus == 0 && output =~ /Event:.*test/
+  end
+
+end
