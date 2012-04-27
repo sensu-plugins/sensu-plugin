b5315b17c6f17f8fb8f8dd97c3cdf26eb2d75b60
Chris Roberts <chrisroberts.code@gmail.com>
2012-04-27 16:18:12 -0700

    Add tests converage for subdue filter

--- test/external_handler_test.rb
+++ test/external_handler_test.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'minitest/autorun'
 require 'json'
+require 'time'
 
 class TestHandlerExternal < MiniTest::Unit::TestCase
   include SensuPluginTestHelper
@@ -17,6 +18,79 @@ class TestHandlerExternal < MiniTest::Unit::TestCase
     }
     output = run_script_with_input(JSON.generate(event))
     assert $?.exitstatus == 0 && output =~ /Event:.*test/
+  end
+
+  def test_missing_keys
+    event = {}
+    output = run_script_with_input(JSON.generate(event))
+    assert $?.exitstatus == 0 && output =~ /Event:/
+  end
+
+end
+
+class TestHandlerExternalWithFilter < MiniTest::Unit::TestCase
+  include SensuPluginTestHelper
+
+  def setup
+    set_script 'external/handle-subdue'
+  end
+
+  def test_subdued
+    event = {
+      'client' => { 'name' => 'test' },
+      'check' => { 'name' => 'test' },
+      'occurrences' => 1,
+      'subdue' => {
+        'start' => (Time.now - 3600).strftime('%l:00 %P').strip,
+        'end' => (Time.now + 3600).strftime('%l:00 %P').strip
+      }
+    }
+    output = run_script_with_input(JSON.generate(event))
+    assert_equal(0, $?.exitstatus)
+    assert_match(/subdue/, output)
+  end
+
+  def test_nonsubdued
+    event = {
+      'client' => { 'name' => 'test' },
+      'check' => { 'name' => 'test' },
+      'occurrences' => 1,
+      'subdue' => {
+        'start' => (Time.now + 3600).strftime('%l:00 %P').strip,
+        'end' => (Time.now + 7200).strftime('%l:00 %P').strip
+      }
+    }
+    output = run_script_with_input(JSON.generate(event))
+    assert $?.exitstatus == 0 && output =~ /Event:/
+  end
+
+  def test_wrapped_subdued
+    event = {
+      'client' => { 'name' => 'test' },
+      'check' => { 'name' => 'test' },
+      'occurrences' => 1,
+      'subdue' => {
+        'start' => (Time.now - 3600).strftime('%l:00 %P').strip,
+        'end' => (Time.now - 7200).strftime('%l:00 %P').strip
+      }
+    }
+    output = run_script_with_input(JSON.generate(event))
+    assert_equal(0, $?.exitstatus)
+    assert_match(/subdue/, output)
+  end
+
+  def test_wrapped_nonsubdued
+    event = {
+      'client' => { 'name' => 'test' },
+      'check' => { 'name' => 'test' },
+      'occurrences' => 1,
+      'subdue' => {
+        'start' => (Time.now + 3600).strftime('%l:00 %P').strip,
+        'end' => (Time.now - 7200).strftime('%l:00 %P').strip
+      }
+    }
+    output = run_script_with_input(JSON.generate(event))
+    assert $?.exitstatus == 0 && output =~ /Event:/
   end
 
   def test_missing_keys
