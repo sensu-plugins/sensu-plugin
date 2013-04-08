061eb796b5413e252e59d11c90fd7112885f07cc
Justin Lambert <jlambert@eml.cc>
2013-04-08 04:27:18 -0600

    ensure to_i on refresh only

--- lib/sensu-handler.rb
+++ lib/sensu-handler.rb
@@ -65,14 +65,14 @@ module Sensu
     end
 
     def filter_repeated
-      occurrences = @event['check']['occurrences'].to_i || 1
-      interval    = @event['check']['interval'].to_i    || 30
-      refresh     = @event['check']['refresh'].to_i     || 1800
+      occurrences = @event['check']['occurrences'] || 1
+      interval    = @event['check']['interval']    || 30
+      refresh     = @event['check']['refresh']     || 1800
       if @event['occurrences'] < occurrences
         bail 'not enough occurrences'
       end
       if @event['occurrences'] > occurrences && @event['action'] == 'create'
-        number = refresh.fdiv(interval).to_i
+        number = refresh.to_i.fdiv(interval).to_i
         unless number == 0 || @event['occurrences'] % number == 0
           bail 'only handling every ' + number.to_s + ' occurrences'
         end
