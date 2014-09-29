9ab288723d6aee19c7d8629501db51e627e7cb92
Sean Porter <portertech@gmail.com>
2014-09-29 12:07:58 -0700

    [check-source] added tests for the event_summary() helper

---
+++ test/handle_helper_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class TestHandleHelpers < MiniTest::Unit::TestCase
+  include SensuPluginTestHelper
+
+  def setup
+    set_script 'external/handle-helpers'
+  end
+
+  def test_event_summary
+    event = {
+      'client' => {
+        'name' => 'test'
+      },
+      'check' => {
+        'name' => 'test',
+        'output' => 'test',
+        'status' => 0
+      },
+      'occurrences' => 1,
+      'action' => 'create'
+    }
+    output = run_script_with_input(JSON.generate(event))
+    assert_equal(0, $?.exitstatus)
+    assert_match("test/test : test\n", output)
+    event['check']['source'] = 'switch-x'
+    output = run_script_with_input(JSON.generate(event))
+    assert_equal(0, $?.exitstatus)
+    assert_match("switch-x/test : test\n", output)
+    event['check']['description'] = 'y is broken'
+    output = run_script_with_input(JSON.generate(event))
+    assert_equal(0, $?.exitstatus)
+    assert_match("y is broken\n", output)
+    event['check']['notification'] = 'z is broken'
+    output = run_script_with_input(JSON.generate(event))
+    assert_equal(0, $?.exitstatus)
+    assert_match("z is broken\n", output)
+  end
+end
