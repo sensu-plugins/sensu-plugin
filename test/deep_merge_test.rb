2b1e428e3b5b16cd7c5c20e8f52a20c1fa19380e
Justin Kolberg <amd.prophet@gmail.com>
2016-06-07 17:40:01 -0700

    [deep_merge_fix] add deep_merge_tests

---
+++ test/deep_merge_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+require 'sensu-plugin/utils'
+
+class TestDeepMerge < MiniTest::Test
+  include SensuPluginTestHelper
+  include Sensu::Plugin::Utils
+
+  def test_hash
+    merged = deep_merge({:a => "a"}, {:b => "b"})
+    assert(merged == {:a => "a", :b => "b"})
+  end
+
+  def test_nested_hash
+    merged = deep_merge({:a => {:b => "c"}}, {:a => {:d => "e"}})
+    assert(merged == {:a => {:b => "c", :d => "e"}})
+  end
+
+  def test_nested_array
+    merged = deep_merge({:a => ["b"]}, {:a => ["c"]})
+    assert(merged, {:a => ["b", "c"]})
+  end
+
+  def test_conflicting_types
+    merged = deep_merge({:a => {:b => "c"}}, {:a => ["d"]})
+    assert(merged, {:a => {:b => "c"}})
+  end
+end
