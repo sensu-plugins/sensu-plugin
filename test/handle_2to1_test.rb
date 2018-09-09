21fca96b6bf5a8ef72ffd2ad70c95d6165ad36b3
Jef Spaleta <jef@sensu.io>
2018-09-08 17:03:24 -0800

    extend 2to1 test coverage to include history

--- test/handle_2to1_test.rb
+++ test/handle_2to1_test.rb
@@ -9,24 +9,8 @@ class TestHandle2to1 < MiniTest::Test
   end
 
   def test_2to1_enabled
-    event = {
-      'entity' => {
-        'id' => 'test_entity',
-        'subscriptions' => ['sub1', 'sub2', 'sub3']
-      },
-      'check' => {
-        'name' => 'test_check',
-        'output' => 'test_output',
-        'subscriptions' => ['sub1', 'sub2', 'sub3'],
-        'proxy_entity_id' => 'test_proxy',
-        'total_state_change' => 1,
-        'state' => 'failing',
-        'status' => 0
-      },
-      'occurrences' => 1,
-      'action' => 'create'
-    }
-    expected = "test_entity : test_check : test_proxy : test_output : 1 : create : sub1|sub2|sub3 : sub1^sub2^sub3\n"
+    event = JSON.parse(fixture('basic_v2_event.json').read)
+    expected = "test_entity : test_check : test_proxy : test_output : 4 : create : sub1|sub2|sub3 : sub1^sub2^sub3 : 01230\n"
     output = run_script_with_input(JSON.generate(event))
     assert_equal(0, $CHILD_STATUS.exitstatus)
     assert_match(expected, output)
