1f6ac7420e389ee6d4ca8fb5f4fbfc6db733f4b4
Jef Spaleta <jef@sensu.io>
2018-11-28 11:56:30 -0900

    simplify event mutation code

--- bin/mutator-sensu-go-into-ruby.rb
+++ bin/mutator-sensu-go-into-ruby.rb
@@ -6,7 +6,6 @@ class Helper < Sensu::Mutator
   def mutate
     STDERR.puts 'WARNING: mutator-sensu-go-into-ruby.rb is meant for primarily for development.'
     STDERR.puts 'Please update your handlers to be compatible with Sensu Go events.'
-    new_event = map_go_event_into_ruby
-    @event = new_event
+    @event = map_go_event_into_ruby
   end
 end
